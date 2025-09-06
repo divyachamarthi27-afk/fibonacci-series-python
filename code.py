c7eed30c3a64b6b0d98af1120e0415181c0aad38
divyasri <divyachamarthi27@gmail.com>
2025-09-06 15:04:22 +0530

    code.py

---
+++ code.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+  fib = [0,1]
+  for i in range(2, abs(n) + 1):
+    fib.append(fib[-1] + fib[-2])
+  if n>= 0:
+    retrun fib[:n+1 ]
+  else:
+    negafib = []
+    for i in range(abs(n) + 1):
+      value = fib[i] if i % 2 == 0 else -fib[i]
+      negafib.append(valuye)
+    return negafib[::-1]
+print("positive fibonacci (n=8):",fibonacci(8))
+print("negative fibonacci (n=-8):",fibonacci(8))
+      
